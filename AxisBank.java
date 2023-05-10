7aac7458957bb803b96a1b8fa4b1eecfeb498b7c
Dhananjeyansarala <130529812+Dhananjeyansarala@users.noreply.github.com>
2023-05-10 22:15:42 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+public class AxisBank extends Bank {
+
+	public long accNum(long num) {
+		
+		return num;
+	}
+
+	
+	public int pinNum(int pin) {
+		
+		return pin;
+	}
+	
+
+}
